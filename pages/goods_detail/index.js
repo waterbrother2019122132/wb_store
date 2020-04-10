e2be717fbd0653d75fe4dc2664989c30ec2fc25e
waterbrother2019122132 <1972352623@qq.com>
2020-04-10 12:16:29 +0800

    test

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,4 +1,5 @@
-// pages/goods_detail/index.js
+import regeneratorRuntime from '../../lib/runtime/runtime';
+import { request } from "../../request/index.js";
 Page({
 
   /**
@@ -6,61 +7,42 @@ Page({
    */
   data: {
 
+    goodsObj:{}
   },
-
+  GoodsInfo:{},
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const{goods_id}=options;
+    console.log(goods_id);
+   this.getGoodsDetail(goods_id);
   },
+  async getGoodsDetail(goods_id){
+    const goodsObj=await request({url:"/goods/detail",data:{goods_id}});
+    this.GoodsInfo=goodsObj;
+    this.setData({
+      goodsObj:{
+        goods_name: goodsObj.goods_name,
+        goods_price: goodsObj.goods_price,
+        // iphone部分手机 不识别 webp图片格式 
+        // 最好找到后台 让他进行修改 
+        // 临时自己改 确保后台存在 1.webp => 1.jpg 
+        goods_introduce: goodsObj.goods_introduce.replace(/\.webp/g, '.jpg'),
+        pics: goodsObj.pics
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+      }
+    })
+    
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handlePreviewImage(e){
+    const urls=this.GoodsInfo.pics.map(v=>v.pics_mid);
+    const current=e.currentTarget.dataset.url;
+    wx.previewImage({
+      current,
+      urls
+    });
   }
+
+
 })
